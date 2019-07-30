ecd5fdf8735b8fe87b832cdecaae85af4b9eb70a
xiaopengwang <xiaopengmonsters@gmail.com>
2019-07-30 14:11:03 +0800

    调整 demo 结构

---
+++ Example/DesignPatterns/生成器/ChasingGame.h
@@ -0,0 +1,16 @@
+//
+//  ChasingGame.h
+//  DesignPatterns
+//
+//  Created by xiaopengwang on 2019/7/30.
+//
+
+#import <Foundation/Foundation.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface ChasingGame : NSObject
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ Example/DesignPatterns/生成器/ChasingGame.m
@@ -0,0 +1,12 @@
+//
+//  ChasingGame.m
+//  DesignPatterns
+//
+//  Created by xiaopengwang on 2019/7/30.
+//
+
+#import "ChasingGame.h"
+
+@implementation ChasingGame
+
+@end
