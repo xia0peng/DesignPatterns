53fbd67a95dff2223e83050a385888e490a6dcf9
xiaopengwang <xiaopengmonsters@gmail.com>
2019-07-31 18:22:07 +0800

    @updata

---
+++ Example/DesignPatterns/桥接/ConsoleCommands.h
@@ -0,0 +1,18 @@
+//
+//  ConsoleCommands.h
+//  DesignPatterns_Example
+//
+//  Created by xiaopengwang on 2019/7/31.
+//  Copyright © 2019 xiaopengwang. All rights reserved.
+//
+
+typedef enum {
+    kConsoleCommandUp,
+    kConsoleCommandDown,
+    kConsoleCommandLeft,
+    kConsoleCommandRight,
+    kConsoleCommandSelect,
+    kConsoleCommandStart,
+    kConsoleCommandAction1,
+    kConsoleCommandAction2
+} ConsoleCommand;
